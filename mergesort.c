8489930922037ec45dac0374ab7ece7c831f0b82
Pedro Costa <pedroo.s.costaa@gmail.com>
2023-11-19 09:52:48 -0300

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -5,13 +5,13 @@
 void mergesort(int vet[], int inicio, int fim);
 
 int main() {
-    int vet[TAM] = {3, 2, 5, 1, 4};
+    int vet[TAM] = {3, 2, 5, 1, 4};//criação do vetor
     printf("Vetor desordenado:\n");
     for (int i = 0; i < TAM; i++) {
         printf("%d ", vet[i]);
     }
 
-    mergesort(vet, 0, TAM - 1);
+    mergesort(vet, 0, TAM - 1);//uso da função
     printf("\nVetor ordenado:\n");
     for (int i = 0; i < TAM; i++) {
         printf("%d ", vet[i]);
@@ -23,18 +23,18 @@ int main() {
 
 void mergesort(int vet[], int inicio, int fim){
     if (inicio < fim) {
-        int meio = (inicio + fim) / 2;
+        int meio = (inicio + fim) / 2;//divisão do vetor
         mergesort(vet, inicio, meio);
         mergesort(vet, meio + 1, fim);
 
-        int i = 0, j = 0, k;
+        int i = 0, j = 0, k;//variaveis auxiliares
         int tam1 = meio - inicio + 1;
         int tam2 = fim - meio;
 
-        int *esquerda = malloc(tam1 * sizeof(int));
+        int *esquerda = malloc(tam1 * sizeof(int));//vetor dividido
         int *direita = malloc(tam2 * sizeof(int));
 
-        for (i = 0; i < tam1; i++)
+        for (i = 0; i < tam1; i++)//atribuição dos vetores divididos
             esquerda[i] = vet[inicio + i];
         for (j = 0; j < tam2; j++)
             direita[j] = vet[meio + 1 + j];
@@ -42,7 +42,7 @@ void mergesort(int vet[], int inicio, int fim){
        
         k = inicio;
 
-        while (i < tam1 && j < tam2) {
+        while (i < tam1 && j < tam2) {//ordenação de fato
             if (esquerda[i] <= direita[j]) {
                 vet[k] = esquerda[i];
                 i++;
